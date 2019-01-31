1e86c8485836824c008aa7d5e44b5d11085a54fb
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-01-31 13:39:44 +0100

    Added 'split part' part

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -2,14 +2,35 @@ function tip_calculator() {
 
     let amount = parseInt(document.querySelector("#amount").value),
         satisfaction = parseInt(document.querySelector('input[name="satisfaction"]:checked').value),
+        persons = parseInt(document.querySelector("#persons").value),
         tipUnrounded = (amount / 100 * satisfaction),
-        tip =  parseInt(tipUnrounded.toFixed(2).replace('.', ',')),
+        tip = parseInt(tipUnrounded.toFixed(2).replace('.', ',')),
         totalUnrounded = (amount + tip),
-        total = totalUnrounded.toFixed(2).replace('.', ',');
+        total = parseInt(totalUnrounded.toFixed(2).replace('.', ',')),
+        eachPerson = (total / persons)
+
+    if (persons > 1) {
+        document.querySelector("#result-eachPerson").style.display = "block"
+    }
 
     document.querySelector("#result-amount").innerHTML = "The amount is " + amount + " &euro;."
     document.querySelector("#result-tip").innerHTML = "The tip is " + tip + " &euro;."
     document.querySelector("#result-total").innerHTML = "The total is " + total + " &euro;."
+    document.querySelector("#result-eachPerson").innerHTML = "Each person pays " + eachPerson + " &euro;."
+}
+
+function splitBill() {
+    // Get the checkbox
+    var checkBox = document.getElementById("split");
+    // Get the output text
+    var text = document.getElementById("split-bill");
+
+    // If the checkbox is checked, display the output text
+    if (checkBox.checked == true) {
+        text.style.display = "block";
+    } else {
+        text.style.display = "none";
+    }
 }
 
 document.querySelector('#calculate').addEventListener('click', tip_calculator);
