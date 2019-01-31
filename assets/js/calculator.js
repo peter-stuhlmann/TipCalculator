be1f418c598b6cabe7794a4440a0ac3b8173802b
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-01-31 12:28:19 +0100

    Added output for amount and total

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -2,12 +2,14 @@ function tip_calculator() {
 
     let amount = parseInt(document.querySelector("#amount").value),
         satisfaction = parseInt(document.querySelector('input[name="satisfaction"]:checked').value),
-        tip = (amount / 100 * satisfaction)
+        tipUnrounded = (amount / 100 * satisfaction),
+        tip =  parseInt(tipUnrounded.toFixed(2).replace('.', ',')),
+        totalUnrounded = (amount + tip),
+        total = totalUnrounded.toFixed(2).replace('.', ',');
 
-    tip.toFixed(2).replace('.', ',')
-
-    document.querySelector("#tip-result").innerHTML = "The tip should be " + tip + " euros."
-
+    document.querySelector("#result-amount").innerHTML = "The amount is " + amount + " &euro;."
+    document.querySelector("#result-tip").innerHTML = "The tip is " + tip + " &euro;."
+    document.querySelector("#result-total").innerHTML = "The total is " + total + " &euro;."
 }
 
 document.querySelector('#calculate').addEventListener('click', tip_calculator);
